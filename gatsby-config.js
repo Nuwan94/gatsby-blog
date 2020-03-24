03348b46da1ddd213e6af4e84832b64bf62764bc
Nuwan94 <nuwansalawatta@gmail.com>
2020-03-24 05:32:12 +0530

    fix gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,32 +4,31 @@ const pxtorem = require('postcss-pxtorem')
 module.exports = {
   pathPrefix: '/',
   siteMetadata: {
-    url: 'https://blog.nuwan.dev/',
+    url: 'https://blog.nuwan.dev',
     title: "Nuwan's Blog",
     subtitle: 'Coder | Tech Enthusiast | Geek',
     copyright: '© All rights reserved.',
     disqusShortname: 'nsa94',
-    menu: [
-      {
-        label: 'Posts',
-        path: '/',
-      },
-      {
-        label: 'Categories',
-        path: '/categories/',
-      },
-      {
-        label: 'Tags',
-        path: '/tags/',
-      },
-      {
-        label: 'About',
-        path: '/about/',
-      },
-      {
-        label: 'Contact',
-        path: '/contact/',
-      },
+    menu: [{
+      label: 'Posts',
+      path: '/',
+    },
+    {
+      label: 'Categories',
+      path: '/categories/',
+    },
+    {
+      label: 'Tags',
+      path: '/tags/',
+    },
+    {
+      label: 'About',
+      path: '/about/',
+    },
+    {
+      label: 'Contact',
+      path: '/contact/',
+    },
     ],
     author: {
       name: 'Nuwan Sameera Alawatta',
@@ -40,36 +39,35 @@ module.exports = {
       github: 'nuwan94',
     },
   },
-  plugins: [
-    {
-      resolve: 'gatsby-plugin-favicon',
-      options: {
-        logo: './src/favicon.png',
-        injectHTML: true,
-        icons: {
-          android: true,
-          appleIcon: true,
-          appleStartup: true,
-          coast: false,
-          favicons: true,
-          firefox: true,
-          twitter: false,
-          yandex: false,
-          windows: false,
-        },
+  plugins: [{
+    resolve: 'gatsby-plugin-favicon',
+    options: {
+      logo: './src/favicon.png',
+      injectHTML: true,
+      icons: {
+        android: true,
+        appleIcon: true,
+        appleStartup: true,
+        coast: false,
+        favicons: true,
+        firefox: true,
+        twitter: false,
+        yandex: false,
+        windows: false,
       },
     },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/src/pages`,
-        name: 'pages',
-      },
+  },
+  {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      path: `${__dirname}/src/pages`,
+      name: 'pages',
     },
-    {
-      resolve: 'gatsby-plugin-feed',
-      options: {
-        query: `
+  },
+  {
+    resolve: 'gatsby-plugin-feed',
+    options: {
+      query: `
           {
             site {
               siteMetadata {
@@ -80,19 +78,25 @@ module.exports = {
             }
           }
         `,
-        feeds: [
-          {
-            serialize: ({ query: { site, allMarkdownRemark } }) =>
-              allMarkdownRemark.edges.map(edge =>
-                Object.assign({}, edge.node.frontmatter, {
-                  description: edge.node.frontmatter.description,
-                  date: edge.node.frontmatter.date,
-                  url: site.siteMetadata.site_url + edge.node.fields.slug,
-                  guid: site.siteMetadata.site_url + edge.node.fields.slug,
-                  custom_elements: [{ 'content:encoded': edge.node.html }],
-                })
-              ),
-            query: `
+      feeds: [{
+        serialize: ({
+          query: {
+            site,
+            allMarkdownRemark,
+          },
+        }) =>
+          allMarkdownRemark.edges.map(edge =>
+            Object.assign({}, edge.node.frontmatter, {
+              description: edge.node.frontmatter.description,
+              date: edge.node.frontmatter.date,
+              url: site.siteMetadata.site_url + edge.node.fields.slug,
+              guid: site.siteMetadata.site_url + edge.node.fields.slug,
+              custom_elements: [{
+                'content:encoded': edge.node.html,
+              }],
+            })
+          ),
+        query: `
               {
                 allMarkdownRemark(
                   limit: 1000,
@@ -117,115 +121,118 @@ module.exports = {
                 }
               }
             `,
-            output: '/rss.xml',
-          },
-        ],
-      },
+        output: '/rss.xml',
+      } ],
     },
-    {
-      resolve: 'gatsby-transformer-remark',
-      options: {
-        plugins: [
+  },
+  {
+    resolve: 'gatsby-transformer-remark',
+    options: {
+      plugins: [{
+        resolve: 'gatsby-remark-images',
+        options: {
+          maxWidth: 960,
+          quality: 80,
+        },
+      },
+      {
+        resolve: 'gatsby-remark-responsive-iframe',
+        options: {
+          wrapperStyle: 'margin-bottom: 1.0725rem',
+        },
+      },
+      'gatsby-remark-prismjs',
+      'gatsby-remark-copy-linked-files',
+      'gatsby-remark-smartypants',
+      ],
+    },
+  },
+  'gatsby-transformer-sharp',
+  'gatsby-plugin-sharp',
+  {
+    resolve: 'gatsby-plugin-google-analytics',
+    options: {
+      trackingId: 'UA-73379983-2',
+    },
+  },
+  {
+    resolve: 'gatsby-plugin-google-fonts',
+    options: {
+      fonts: ['roboto:400,400i,500,700'],
+    },
+  },
+  {
+    resolve: 'gatsby-plugin-sitemap',
+    options: {
+      query: `
           {
-            resolve: 'gatsby-remark-images',
-            options: {
-              maxWidth: 960,
-              quality: 80,
-            },
-          },
-          {
-            resolve: 'gatsby-remark-responsive-iframe',
-            options: { wrapperStyle: 'margin-bottom: 1.0725rem' },
-          },
-          'gatsby-remark-prismjs',
-          'gatsby-remark-copy-linked-files',
-          'gatsby-remark-smartypants',
-        ],
-      },
-    },
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
-    {
-      resolve: 'gatsby-plugin-google-analytics',
-      options: { trackingId: 'UA-73379983-2' },
-    },
-    {
-      resolve: 'gatsby-plugin-google-fonts',
-      options: {
-        fonts: ['roboto:400,400i,500,700'],
-      },
-    },
-    {
-      resolve: 'gatsby-plugin-sitemap',
-      options: {
-        query: `
-            {
-              site {
-                siteMetadata {
-                  url
-                }
+            site {
+              siteMetadata {
+                siteUrl: url
               }
-              allSitePage(
-                filter: {
-                  path: { regex: "/^(?!/404/|/404.html|/dev-404-page/)/" }
-                }
-              ) {
-                edges {
-                  node {
-                    path
-                  }
-                }
-              }
-          }`,
-        output: '/sitemap.xml',
-        serialize: ({ site, allSitePage }) =>
-          allSitePage.edges.map(edge => {
-            return {
-              url: site.siteMetadata.url + edge.node.path,
-              changefreq: 'daily',
-              priority: 0.7,
             }
-          }),
-      },
+            allSitePage(
+              filter: {
+                path: { regex: "/^(?!/404/|/404.html|/dev-404-page/)/" }
+              }
+            ) {
+              edges {
+                node {
+                  path
+                }
+              }
+            }
+          }
+        `,
+      output: '/sitemap.xml',
+      serialize: ({
+        site,
+        allSitePage,
+      }) => allSitePage.edges.map(edge => ({
+        url: site.siteMetadata.siteUrl + edge.node.path,
+        changefreq: 'daily',
+        priority: 0.7,
+      })),
     },
-    'gatsby-plugin-offline',
-    'gatsby-plugin-catch-links',
-    'gatsby-plugin-react-helmet',
-    {
-      resolve: 'gatsby-plugin-sass',
-      options: {
-        postCssPlugins: [
-          lost(),
-          pxtorem({
-            rootValue: 16,
-            unitPrecision: 5,
-            propList: [
-              'font',
-              'font-size',
-              'line-height',
-              'letter-spacing',
-              'margin',
-              'margin-top',
-              'margin-left',
-              'margin-bottom',
-              'margin-right',
-              'padding',
-              'padding-top',
-              'padding-left',
-              'padding-bottom',
-              'padding-right',
-              'border-radius',
-              'width',
-              'max-width',
-            ],
-            selectorBlackList: [],
-            replace: true,
-            mediaQuery: false,
-            minPixelValue: 0,
-          }),
-        ],
-        precision: 8,
-      },
+  },
+  'gatsby-plugin-offline',
+  'gatsby-plugin-catch-links',
+  'gatsby-plugin-react-helmet',
+  {
+    resolve: 'gatsby-plugin-sass',
+    options: {
+      postCssPlugins: [
+        lost(),
+        pxtorem({
+          rootValue: 16,
+          unitPrecision: 5,
+          propList: [
+            'font',
+            'font-size',
+            'line-height',
+            'letter-spacing',
+            'margin',
+            'margin-top',
+            'margin-left',
+            'margin-bottom',
+            'margin-right',
+            'padding',
+            'padding-top',
+            'padding-left',
+            'padding-bottom',
+            'padding-right',
+            'border-radius',
+            'width',
+            'max-width',
+          ],
+          selectorBlackList: [],
+          replace: true,
+          mediaQuery: false,
+          minPixelValue: 0,
+        }),
+      ],
+      precision: 8,
     },
+  },
   ],
 }
